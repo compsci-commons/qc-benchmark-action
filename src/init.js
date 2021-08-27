294a824ae96c9d02e151b404798e2b6ef0e63c25
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-08-27 22:06:46 +0200

    init micromamba

--- src/init.js
+++ src/init.js
@@ -4,12 +4,12 @@ async function doInit () {
   const benchmarkName = common.getBenchmarkName()
   console.log(`Download data for ${benchmarkName}.`)
 
+  await common.initMicromamba()
+
   const envpath = common.getBenchmarkFile('download-env.yaml')
   const envname = 'download-env'
   const prefix = common.getEnvActivate(envpath, envname)
   await common.initEnv(envpath, envname)
-
-  await common.initMicromamba()
 
   await common.exec(`${prefix}; ${common.getOutpathEnvvars()} bash ${common.getBenchmarkFile('download.sh')}`)
 }
