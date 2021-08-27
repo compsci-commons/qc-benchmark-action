5d0d7f4a47e59e3ea4a3c9044fee34098e3e9e67
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-08-27 23:27:52 +0200

    minor

--- src/common.js
+++ src/common.js
@@ -14,7 +14,7 @@ fs.mkdirSync(benchmarkOutdir, { recursive: true })
 fs.closeSync(fs.openSync(bashrc, 'w'))
 
 async function _exec (cmd) {
-  await exec.exec('bash', ['-l', '-c', `source ${bashrc}; MAMBA_ROOT_PREFIX=${mambaRootPrefix} ${cmd}`])
+  await exec.exec('bash', ['-l', '-c', `source ${bashrc}; MAMBA_ROOT_PREFIX=${mambaRootPrefix} MAMBA_EXE=${micromamba} ${cmd}`])
 }
 
 let meta = `benchmarks/${benchmarkName}/meta.yaml`
