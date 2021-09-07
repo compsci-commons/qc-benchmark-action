814a625fd916bf6b42ac65ed057e5fd9d10d76a1
Johannes Köster <johannes.koester@tu-dortmund.de>
2021-09-07 17:48:08 +0200

    intersect confidence regions with target regions

--- benchmarks/giab-na12878-exome/download.sh
+++ benchmarks/giab-na12878-exome/download.sh
@@ -16,9 +16,25 @@ bcftools view \
 # download confidence bed
 curl --insecure -L \
     https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/NA12878_HG001/latest/GRCh38/HG001_GRCh38_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-X_v.3.3.2_highconf_nosomaticdel_noCENorHET7.bed \
-    > $confidence
+    > $confidence_regions_all
+
+# download liftover
+curl --insecure -L \
+    http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz \
+    > $liftover
+
+# download target regions
+liftOver \
+    <(curl --insecure -L ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Nebraska_NA12878_HG001_TruSeq_Exome/TruSeq_exome_targeted_regions.hg19.bed) \
+    $liftover \
+    $target_regions \
+    /dev/null
+
+# intersect target and confidence regions
+bedtools intersect -a $confidence_regions_all -b $target_regions > $confidence_regions
 
 # download reference genome
 curl --insecure -L \
-    https://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.21.fa.gz \
+    https://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.21.fa.gz | \
+    gzip --decompress --stdout
     > $reference
--- benchmarks/giab-na12878-exome/eval.sh
+++ benchmarks/giab-na12878-exome/eval.sh
@@ -3,4 +3,4 @@ IFS=$'\n\t'
 
 samtools faidx $reference
 
-hap.py $truth $results -f $confidence -o $report -r $reference --write-vcf
+hap.py $truth $results -f $confidence_regions -o $report -r $reference --write-vcf
